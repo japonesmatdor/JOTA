00296b4cbe1de5a60ca176e51c35761eb9038274
japonesmatdor <joao.goncalves.cunha@escola.pr.gov.br>
2026-06-09 08:56:31 -0300

    Add files via upload

---
+++ java script 2/script.js
@@ -0,0 +1,34 @@
+//var 
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento = 7;
+
+// if (forma === 'retangulo') {
+ //   area = altura * comprimento;
+// } else {
+  //  area = (altura * comprimento) / 2;
+// }
+
+// area = altura * comprimento;
+// console.log(area)
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'triângulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
